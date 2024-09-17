c5b4bacb96900a554339018684f614a49ad9295b
pettroch <sidoro-petya@bk.ru>
2024-09-17 12:04:25 +1000

    Сделан CRUD

--- src/api/api.py
+++ src/api/api.py
@@ -1,5 +1,4 @@
-import uvicorn
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from api import crud
@@ -13,3 +12,38 @@ app = FastAPI()
 @app.post("/notes/")
 def create_note_endpoint(note: NoteCreate, db: Session = Depends(get_db)):
     return crud.create_note(db=db, note=note)
+
+
+@app.get("/notes")
+def read_notes(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    return crud.get_notes(db=db, skip=skip, limit=limit)
+
+
+@app.get("/notes/{note_id}")
+def read_note(note_id: int, db: Session = Depends(get_db)):
+    db_note = crud.get_note_by_id(db, note_id)
+
+    if db_note is None:
+        raise HTTPException(status_code=404, detail="Note not found")
+    
+    return db_note
+
+
+@app.delete("/notes/{note_id}")
+def delete_note_endpoint(note_id: int, db: Session = Depends(get_db)):
+    db_note = crud.delete_note(db=db, note_id=note_id)
+
+    if db_note is None:
+        raise HTTPException(status_code=404, detail="Note not found")
+    
+    return {"detail": "Note deleted successfully"}
+
+
+@app.put("/notes/{note_id}")
+def update_note_endpoint(note_id: int, note: NoteUpdate, db: Session = Depends(get_db)):
+    db_note = crud.update_note(db=db, note_id=note_id, note=note)
+
+    if db_note is None:
+        raise HTTPException(status_code=404, detail="Note not found")
+    
+    return db_note
--- src/api/crud.py
+++ src/api/crud.py
@@ -15,3 +15,30 @@ def create_note(db: Session, note: NoteCreate):
     db.refresh(db_note)
 
     return db_note
+
+
+def get_notes(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(Note).offset(skip).limit(limit).all()
+
+
+def get_note_by_id(db: Session, note_id: int):
+    return db.query(Note).filter(Note.id == note_id).first()
+
+
+def delete_note(db: Session, note_id: int):
+    db_note = get_note_by_id(db, note_id)
+    if db_note:
+        db.delete(db_note)
+        db.commit()
+    return db_note
+
+
+def update_note(db: Session, note_id: int, note: NoteUpdate):
+    db_note = get_note_by_id(db, note_id)
+    if db_note:
+        db_note.title = note.title
+        db_note.content = note.content
+        db_note.tags = ",".join(note.tags)
+        db.commit()
+        db.refresh(db_note)
+    return db_note
